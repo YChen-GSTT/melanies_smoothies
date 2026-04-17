9c549f8adff5b3ff4f946d63bbcd6653a18cc47c
YChen-GSTT <153197258+YChen-GSTT@users.noreply.github.com>
2026-04-17 18:54:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,8 +45,9 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
 
-smoothiefroot_response = requests.get("https://https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response).json()
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+#st.text(smoothiefroot_response).json()
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 contact_option = st.selectbox(
     "How would you like to be contacted?",
